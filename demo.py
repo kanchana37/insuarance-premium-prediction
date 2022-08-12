b1a5bfe8f3e71198c47308530dd0d9cfa7e306c2
Aryan Chopra <kanchanachopra376@gmail.com>
2022-08-13 02:13:33 +0530

    added

--- demo.py
+++ demo.py
@@ -2,20 +2,27 @@ from insurance.pipeline.pipeline import Pipeline
 from insurance.exception import PackageException
 from insurance.logger import logging
 from insurance.config.configuration import Configuration
-import sys
 import os
 
 def main():
     try:
         config_path = os.path.join("config","config.yaml")
         pipeline = Pipeline(Configuration(config_file_path=config_path))
+        #pipeline.run_pipeline()
         pipeline.start()
         logging.info("main function execution completed.")
+        # # data_validation_config = Configuartion().get_data_transformation_config()
+        # # print(data_validation_config)
+        # schema_file_path=r"C:\Users\aryan\insurance premium prediction\config\schema.yaml"
+        # file_path=
+
+        # df= DataTransformation.load_data(file_path=file_path,schema_file_path=schema_file_path)
+        # print(df.columns)
+        # print(df.dtypes)
 
     except Exception as e:
-        raise PackageException(e, sys) from e
         logging.error(f"{e}")
         print(e)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
